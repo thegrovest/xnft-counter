eb9a079fb92070a52da8baa4b5847c7d344edbed
thegrovest <grovest.nft@gmail.com>
2023-08-10 22:07:27 -0600

    kanye was here

--- src/hooks/useCountdown.tsx
+++ src/hooks/useCountdown.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 
+
 const useCountdown = (targetDate) => {
   const countDownDate = new Date(targetDate).getTime();
 
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -17,7 +17,7 @@ export function HomeScreen() {
     "theme hook with light/dark support",
   ];
 
-  const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
+  const THREE_DAYS_IN_MS = 7 * 24 * 60 * 60 * 1000;
   const NOW_IN_MS = new Date().getTime();
 
   const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
